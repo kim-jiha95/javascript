ae1b8b83426f47d8c70b2a6ba44bac0b5b46f10f
kim-jiha95 <kimjiha1112@gmail.com>
2021-12-07 23:48:07 +0900

    type test

---
+++ cleancode/type.js
@@ -0,0 +1,21 @@
+// function Person(name, age){
+//     this.name = name;
+//     this.name = age;
+// }
+
+// const p = {
+
+// }
+
+const arr = []
+const func = function() {
+
+}
+
+const date = new Date()
+
+arr instanceof Object
+func instanceof Object
+date instanceof Object
+
+Object.prototype.toString.call(new String(''))
